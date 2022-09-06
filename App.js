ffb64db78c22ea1a74d68da8f33faee1688c94d4
Marc <marc@unit37.org>
2022-09-06 20:47:21 +0300

    fix everything

--- App.js
+++ App.js
@@ -1,19 +1,139 @@
 import React from 'react';
-import Home from './Screens/Home';
-import History from './Screens/History';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+
 const Stack = createNativeStackNavigator();
+
+function HomeScreen({ navigation }) {
+
+  const [data, setData] = React.useState([]);
+  const [result, setResult] =  React.useState(null)
+  
+  const [inputValue1, onChangeValue1] = React.useState(0);
+  const [inputValue2, onChangeValue2] = React.useState(0);
+
+  const minusButtonPressed = () => {
+    let calcResult = parseInt(inputValue1) - parseInt(inputValue2)
+    setResult(calcResult)
+    setData([...data, { key: inputValue1 + " - " + inputValue2 + " = " + calcResult }]);
+  }
+
+  const plusButtonPressed = () => {
+    let calcResult = parseInt(inputValue1) + parseInt(inputValue2)
+    setResult(calcResult)
+    setData([...data, { key: inputValue1 + " + " + inputValue2 + " = " + calcResult }]);
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+    },
+    buttonContainer: {
+      flexDirection: "row",
+      alignItems: 'center',
+      justifyContent: 'space-between'
+    },
+    text: {
+      marginTop: 50,
+      marginBottom: 5,
+    },
+    button: {
+      width: '40%',
+      height: 40
+    },
+    input: {
+      marginBottom: 5,
+      width: 200,
+      borderColor: 'gray',
+      borderWidth: 1 
+    }
+  });
+
+
+  return (
+    <View style={styles.container}>
+      
+      <Text id="resultText" style = {styles.text}>
+        Result: {result}
+      </Text>
+      <TextInput 
+        id="inputValue1" 
+        style={styles.input}
+        onChangeText={onChangeValue1}
+        value={inputValue1}
+        keyboardType="numeric"
+        placeholder='First value'
+      />
+      <TextInput 
+        id="inputValue2" 
+        style={styles.input}
+        onChangeText={onChangeValue2}
+        value={inputValue2}
+        keyboardType="numeric"
+        placeholder='Second value'
+      />
+      <View style={styles.buttonContainer}>
+        <Button 
+          title="+"
+          onPress={plusButtonPressed} 
+          style={styles.button}
+        />
+        <Button 
+          title="-" 
+          onPress={minusButtonPressed} 
+          style={styles.button}
+        />  
+        <Button
+        title="History"
+        style={styles.button}
+        onPress={() => 
+          navigation.navigate('History', {data})
+        }
+      />
+      </View>
+    </View>
+  );
+}
+
+function HistoryScreen({ route }) {
+  console.log(route.params);
+  const  {data} = route.params;
+  return (
+    <View style={styles.container}>
+      <Text style = {styles.text}>
+        History
+      </Text>
+      <FlatList style={styles.list}
+        data={data}
+        renderItem={({ item }) =>
+          <Text>{item.key}</Text>
+        }
+      />
+    </View>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen  name="Home" component={Home} />
-        <Stack.Screen  name="History" component={History} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="History" component={HistoryScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
